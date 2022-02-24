06c90ed0fa6e37c8febc517176370b6cbf52b461
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-02-24 14:36:41 +0100

    Updates

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -119,34 +119,106 @@ const Portfolio = () => {
           </div>
           <div className="card middle">
             <div className="front">
-            <img src = {projectPicture4} alt="Project4" />
+            <img src = {projectPicture3} alt="Project3" />
             </div>
             <div className="back">
               <div className="back-content middle">
-                <h2>New biz</h2>
+                <h2>Feedback UI</h2>
+                <span>
+                  Feedback UI is a visually aesthetic web application that
+                  collects feedback data from users, that consists of a number
+                  value on a scale of 1 to 10 and a feedback text.{" "}
+                </span>
+                <div className="btn-main">
+                  <button className="btn-3">
+                    <a
+                      href="https://github.com/Godsheritage/Repair"
+                      target="_blank" rel = 'noreferrer'
+                    >
+                      View Code
+                    </a>
+                  </button>
+                  <button className="btn-3-1">
+                    <a
+                      href="https://github.com/Godsheritage/Repair"
+                      target="_blank" rel = 'noreferrer'
+                    >
+                      View Application
+                    </a>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
           <div className="card middle">
             <div className="front">
-            <img src = {projectPicture5} alt="Project5" />
+            <img src = {projectPicture3} alt="Project3" />
             </div>
             <div className="back">
               <div className="back-content middle">
-                <h2>Roller trade ventures</h2>
+                <h2>Feedback UI</h2>
+                <span>
+                  Feedback UI is a visually aesthetic web application that
+                  collects feedback data from users, that consists of a number
+                  value on a scale of 1 to 10 and a feedback text.{" "}
+                </span>
+                <div className="btn-main">
+                  <button className="btn-3">
+                    <a
+                      href="https://github.com/Godsheritage/Repair"
+                      target="_blank" rel = 'noreferrer'
+                    >
+                      View Code
+                    </a>
+                  </button>
+                  <button className="btn-3-1">
+                    <a
+                      href="https://github.com/Godsheritage/Repair"
+                      target="_blank" rel = 'noreferrer'
+                    >
+                      View Application
+                    </a>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
           <div className="card middle">
             <div className="front">
-            <img src = {projectPicture6} alt="Project6" />
+            <img src = {projectPicture3} alt="Project3" />
             </div>
             <div className="back">
               <div className="back-content middle">
-                <h2>Bounty trade ventures</h2>
+                <h2>Github Profile Finder</h2>
+                <span>
+                  Feedback UI is a visually aesthetic web application that
+                  collects feedback data from users, that consists of a number
+                  value on a scale of 1 to 10 and a feedback text.{" "}
+                  Github Finder is a web application for retrieving github profile of users upon search
+                  *Integrated Github Rest API 
+                </span>
+                <div className="btn-main">
+                  <button className="btn-3">
+                    <a
+                      href="https://github.com/Godsheritage/Repair"
+                      target="_blank" rel = 'noreferrer'
+                    >
+                      View Code
+                    </a>
+                  </button>
+                  <button className="btn-3-1">
+                    <a
+                      href="https://github.com/Godsheritage/Repair"
+                      target="_blank" rel = 'noreferrer'
+                    >
+                      View Application
+                    </a>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
+         
         </div>
       </div>
     </section>
